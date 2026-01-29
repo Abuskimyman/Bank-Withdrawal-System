6e5ef04a823c3eac8d55868399984353c1793258
Ekeanyanwu Chukwuebuka Christian <ekeanyanwuchristian10@gmail.com>
2026-01-29 13:29:10 +0100

    Add files via upload

---
+++ BankWithdrawalSystem.py
@@ -0,0 +1,27 @@
+# try:
+#     pass
+# except Exception:
+#     pass
+
+
+
+balance = 100000  # initial balance
+
+try:
+    amount = float(input("Enter amount to withdraw: ₦"))
+
+    if amount > balance:
+        raise ValueError("Insufficient funds! Withdrawal exceeds balance.")
+
+    balance = balance - amount
+
+except ValueError as e:
+    print("Transaction Failed:", e)
+
+else:
+    print(f"Withdrawal successful!")
+    print(f"Amount withdrawn: ₦{amount}")
+    print(f"Remaining balance: ₦{balance}")
+
+finally:
+    print("Transaction logged. Thank you for using our bank service.")
